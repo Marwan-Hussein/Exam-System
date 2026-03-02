506aef31a1db14edf3e14f4ae2e329d237097b91
marwan hussein <marawanlabana649@gmail.com>
2026-03-02 02:31:44 +0200

    adding QuestionInfo as data structure

--- ExaminationSystem/Classes/AnswerList.cs
+++ ExaminationSystem/Classes/AnswerList.cs
@@ -4,13 +4,26 @@ using System.Linq;
 
 namespace ExaminationSystem.Classes
 {
+    internal struct QuestionInfo
+    {
+        public QuestionInfo(int id, double mark, bool isCorrect)
+        {
+            Id = id;
+            Mark = mark;
+            IsCorrect = isCorrect;
+        }
+
+        public int Id {  get; set; }
+        public double Mark {  get; set; }
+        public bool IsCorrect { get; set; }
+    }
     // answers for one exam
     internal class AnswerList
     {
-        internal List<Answer> Answers {  get; set; }
+        internal List<Answer> Answers { get; set; }
 
-        internal AnswerList() 
-            { Answers = new List<Answer>(); }
+        internal AnswerList()
+        { Answers = new List<Answer>(); }
 
         internal void AddAnswer(Answer answer)
         {
@@ -25,7 +38,7 @@ namespace ExaminationSystem.Classes
     internal class AnswerSet
     {
         private List<Answer> Answers { get; set; }
-        internal DateTime SubmissionTime {  get; set; }
+        internal DateTime SubmissionTime { get; set; }
         internal int StudentId { get; set; }
 
         internal AnswerSet(int studentId)
@@ -37,13 +50,31 @@ namespace ExaminationSystem.Classes
 
         internal void AddAnswer(Answer answer) { Answers.Add(answer); }
 
-        internal Answer GetAnswer(int )
+        internal Answer GetAnswer(int questionId)
+            => Answers.FirstOrDefault(q => q.QuestionId == questionId);
+        
         internal int CalculateScore(QuestionList questions)
         {
-            int score = 0;
-            foreach (var (q , ans) in questions.Zip(Answers, (x,y) => (x,y)))
-                if(ans.IsCorrect(q))
-                    ++score;
-            return score;
+            double score = 0;
+
+            foreach (Answer ans in Answers)
+            {
+                Question question = questions.FirstOrDefault(q => q.QuestionId == ans.QuestionId);
+                if (question != null && ans.IsCorrect(question))
+                    score += question.Marks;
+            }
+            return (int)Math.Round(score);
         }
+        internal List<QuestionInfo> GetResults(QuestionList questions)
+        {
+            var result = new List<QuestionInfo>();
+            foreach (Answer ans in Answers)
+            {
+                Question question = questions.FirstOrDefault(q => q.QuestionId == ans.QuestionId);
+                if (question != null)
+                    result.Add(new QuestionInfo(question.QuestionId.Value, question.Marks ,ans.IsCorrect(question)));
+            }
+            return result;
+        }
+    }
 }
