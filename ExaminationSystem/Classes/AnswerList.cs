cc861fb7a215cff9f275355b713aa656dbcf517f
marwan hussein <marawanlabana649@gmail.com>
2026-03-02 02:11:39 +0200

    AnswerList (completed) start in building AnswerSet

--- ExaminationSystem/Classes/AnswerList.cs
+++ ExaminationSystem/Classes/AnswerList.cs
@@ -1,23 +1,43 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ExaminationSystem.Classes
 {
+    // answers for one exam
     internal class AnswerList
     {
-        internal List<Answer> Answers;
+        internal List<Answer> Answers {  get; set; }
 
-        internal AnswerList() { }
+        internal AnswerList() 
+            { Answers = new List<Answer>(); }
+
         internal void AddAnswer(Answer answer)
         {
-
+            Answers.Add(answer);
         }
-
         internal Answer GetAnswer(int questionId)
+            => Answers.FirstOrDefault(q => q.QuestionId == questionId);
+
+    }
+
+    // answers for one student in one exam
+    internal class AnswerSet
+    {
+        private List<Answer> Answers { get; set; }
+        internal DateTime SubmissionTime {  get; set; }
+        internal int StudentId { get; set; }
+
+        internal AnswerSet(int studentId)
         {
-            return Answers[questionId];
+            Answers = new List<Answer>();
+            SubmissionTime = DateTime.Now;
+            StudentId = studentId;
         }
 
+        internal void AddAnswer(Answer answer) { Answers.Add(answer); }
+
+        internal Answer GetAnswer(int )
         internal int CalculateScore(QuestionList questions)
         {
             int score = 0;
@@ -26,5 +46,4 @@ namespace ExaminationSystem.Classes
                     ++score;
             return score;
         }
-    }
 }
