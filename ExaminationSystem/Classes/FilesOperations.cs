2f5d70b90a47bc107340a67ad46e01c546379e3c
marwan hussein <marawanlabana649@gmail.com>
2026-03-02 06:11:52 +0200

    FileOpertions class

--- ExaminationSystem/Classes/FilesOperations.cs
+++ ExaminationSystem/Classes/FilesOperations.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using static System.Console;
 namespace ExaminationSystem.Classes
 {
     internal class FilesOperations
@@ -7,19 +9,41 @@ namespace ExaminationSystem.Classes
         internal static string ProjectPath{ private set; get; }
         internal static string ExamsFolderPath{ private set; get; }
         internal static string AnswersFolderPath{ private set; get; }
+        internal static string StudentsFolderPath{ private set; get; }
         #endregion
+
+        static FilesOperations() { CreateMainFolders(); }
 
         // to create exams, answers folders if not exists
         internal static void CreateMainFolders()
         {
-            string currPath = Directory.GetCurrentDirectory(); // path\\ExaminationSystem\bin\Debug\net5.0
-            ProjectPath = GetNthParent(currPath,3);
-            ExamsFolderPath = Path.Combine(ProjectPath, "Exams");
-            AnswersFolderPath = Path.Combine(ProjectPath, "Answers");
-            if (!Directory.Exists(ExamsFolderPath))
-                Directory.CreateDirectory(ExamsFolderPath);
-            if (!Directory.Exists(AnswersFolderPath))
-                Directory.CreateDirectory(AnswersFolderPath);
+            try {
+                string currPath = Directory.GetCurrentDirectory(); 
+                // path\\ExaminationSystem\bin\Debug\net5.0
+                
+                ProjectPath = GetNthParent(currPath, 3);
+                ExamsFolderPath = Path.Combine(ProjectPath, "Exams");
+                AnswersFolderPath = Path.Combine(ProjectPath, "Answers");
+                StudentsFolderPath = Path.Combine(ProjectPath, "Students");
+
+                CreateDirectoryIfNotExists(ExamsFolderPath);
+                CreateDirectoryIfNotExists(AnswersFolderPath);
+                CreateDirectoryIfNotExists(StudentsFolderPath);
+            }
+            catch(Exception ex)
+            {
+                WriteLine($"Error Creating Folders: {ex.Message}");
+
+                ProjectPath = Directory.GetCurrentDirectory();
+                ExamsFolderPath = Path.Combine(ProjectPath, "Exams");
+                AnswersFolderPath = Path.Combine(ProjectPath, "Answers");
+                StudentsFolderPath = Path.Combine(ProjectPath, "Students");
+
+                CreateDirectoryIfNotExists(ExamsFolderPath);
+                CreateDirectoryIfNotExists(AnswersFolderPath);
+                CreateDirectoryIfNotExists(StudentsFolderPath);
+            }
+
         }
 
         private static string GetNthParent(string path, int n=1)
@@ -34,7 +58,19 @@ namespace ExaminationSystem.Classes
             return dir.FullName;
         }
 
+        private static void CreateDirectoryIfNotExists(string path)
+        {
+            if(!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+        }
+
         internal static string GetAnswersFolder() => AnswersFolderPath;
         internal static string GetExamsFolder() => ExamsFolderPath;
+        internal static string GetStudentsFolder() => StudentsFolderPath;
+
+        internal static string GetExamFilePath(string examName)
+            => Path.Combine(ExamsFolderPath, $"{examName}_{DateTime.Now:yyMMdd_HHmmss}.txt");
+        internal static string GetStudentAnswerPath(int studentId, int examId)
+            => Path.Combine(AnswersFolderPath, $"Student{studentId}_Exam{examId}.txt");
     }
 }
