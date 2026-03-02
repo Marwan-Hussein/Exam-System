7e86be667adc9b259745d93e9082aa9434142f03
marwan hussein <marawanlabana649@gmail.com>
2026-03-02 05:54:08 +0200

    Subject class (COMPLETED)

--- ExaminationSystem/Classes/Subject.cs
+++ ExaminationSystem/Classes/Subject.cs
@@ -1,27 +1,36 @@
-﻿namespace ExaminationSystem.Classes
+﻿using System;
+
+namespace ExaminationSystem.Classes
 {
-    internal class Subject
+    internal class Subject : ICloneable, IComparable<Subject>
     {
+        #region properties
+        internal int SubjectId { get; set; }
         internal string SubjectName { get; set; }
         internal string ProfessorName { get; set; }
 
+        #endregion
+
         #region ctors
         internal Subject() { }
-        internal Subject(string subName, string profName)
+        internal Subject(int id, string subName, string profName)
         {
+            SubjectId = id;
             SubjectName = subName;
             ProfessorName = profName;
         }
         internal Subject(Subject other)
         {
+            SubjectId = other.SubjectId;
             SubjectName = other.SubjectName;
             ProfessorName = other.ProfessorName;
         }
         #endregion
 
+        #region IFaces and overrides
         public override string ToString()
         {
-            return $"Name: {SubjectName}\t Prof: {ProfessorName}";
+            return $"ID:{SubjectId}\t Name: {SubjectName}\t Prof: {ProfessorName}";
         }
 
         public override bool Equals(object obj)
@@ -30,5 +39,19 @@
                 return SubjectName == sub.SubjectName && ProfessorName == sub.ProfessorName;
             return false;
         }
+
+        public override int GetHashCode()
+        {
+            return SubjectId.GetHashCode();
+        }
+
+        public object Clone() => new Subject(this);
+        public int CompareTo(Subject other)
+        {
+            if(other == null)
+                return 1;
+            return SubjectId.CompareTo(other.SubjectId);
+        }
+        #endregion
     }
 }
