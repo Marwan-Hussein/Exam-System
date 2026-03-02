5b05b48e3703f46f39225fb67f145cf7774ff36a
marwan hussein <marawanlabana649@gmail.com>
2026-03-02 05:47:42 +0200

    ExamEventArgs class [inhirated from the base EventArgs]

--- ExaminationSystem/Classes/ExamEventArgs.cs
+++ ExaminationSystem/Classes/ExamEventArgs.cs
@@ -2,17 +2,23 @@
 
 namespace ExaminationSystem.Classes
 {
-    internal class ExamEventArgs
+    internal class ExamEventArgs : EventArgs
     {
         internal ExamMode CurrentMode {  get; set; }
         internal string ExamName { get; set; }
         internal DateTime NotificationTime {  get; set; }
+        internal string Message {  get; set; }
 
         internal ExamEventArgs(ExamMode mode, string name)
         {
             CurrentMode = mode;
             ExamName = name;
             NotificationTime = DateTime.Now;
+            Message = $"Exam {name} is now {mode}";
         }
+
+        internal ExamEventArgs(ExamMode mode, string name, string message)
+            : this(mode, name)
+        { Message = message; }
     }
 }
